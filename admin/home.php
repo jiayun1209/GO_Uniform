32d771a9ab083104edc4f5900b12d52bcf0284ba
jiayun1209 <User-PC@LAPTOP-FNMDPHLU.mshome.net>
2021-10-26 17:30:02 +0800

    change some user & PO part

--- admin/home.php
+++ admin/home.php
@@ -19,7 +19,7 @@
         </div>
         <!-- /.info-box -->
     </div>
-    <div class="col-6 col-sm-4 col-md-3">
+    <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box">
             <span class="info-box-icon bg-gradient-lightblue elevation-1"><i class="fas fa-comments-dollar"></i></span>
 
