a7752c9a91ae60c1c4e9fa15ed0f7ad104d2812a
Ming Nee <mingneeyap@gmail.com>
2021-10-29 00:08:05 +0800

    contract done view function oni

--- admin/contract_mgm/index.php
+++ admin/contract_mgm/index.php
@@ -19,20 +19,74 @@
                         <col width="10%">
                         <col width="10%">
                         <col width="15%">
+                        <col width="10%">
+                        <col width="10%">
                         <col width="15%">
+                    </colgroup>
+                    <thead>
+                    <p> <strong> Sub Contractor List </strong></p>
+                    <tr class="bg-navy disabled">
+                        <th>#</th>
+                        <th>Date Created</th>
+                        <th>Contract ID</th>
+                        <th>Sub-Contractor ID</th>                            
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        $qry = $conn->query("SELECT * FROM `contract` where subcontractor_ID != 0");
+                        while ($row = $qry->fetch_assoc()):
+                            ?>
+                            <tr>
+                                <td class="text-center"><?php echo $i++; ?></td>
+                                <td class="text-center"><?php echo date("M d,Y H:i", strtotime($row['date_created'])); ?></td>
+                                <td class="text-center"><?php echo $row['contract_ID'] ?></td>
+                                <td class="text-center"><?php echo $row['subcontractor_ID'] ?></td>                                
+                                <td class="text-right"><?php echo $row['startDate'] ?></td>
+                                <td class="text-right"><?php echo $row['endDate'] ?></td>
+
+                                <td align="center">
+                                    <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                                        Action
+                                        <span class="sr-only">Toggle Dropdown</span>
+                                    </button>
+                                    <div class="dropdown-menu" role="menu">
+                                        <a class="dropdown-item" href="?page=contract_mgm/view_consub&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-primary"></span> View</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="?page=contract_mgm/manage_con&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>  </div>    
+
+        <div class="container-fluid">
+            <div class="container-fluid">
+                <table class="table table-hover table-striped">
+                    <colgroup>
+                        <col width="5%">
+                        <col width="10%">
+                        <col width="10%">
                         <col width="15%">
                         <col width="10%">
                         <col width="10%">
                         <col width="15%">
                     </colgroup>
                     <thead>
+                        <p> <strong> Vendor List </strong></p>
                         <tr class="bg-navy disabled">
                             <th>#</th>
                             <th>Date Created</th>
                             <th>Contract ID</th>
-                            <th>Sub-Contractor ID</th>
-                            <th>Vendor Name</th>
-                            <th>Staff Name</th>
+                            <th>Vendor ID</th>
                             <th>Start Date</th>
                             <th>End Date</th>
                             <th>Action</th>
@@ -41,24 +95,75 @@
                     <tbody>
                         <?php
                         $i = 1;
-                        $qry = $conn->query("SELECT c.*,v.*,s.* FROM `contract` c,vendor v, staff s where c.vendor_ID = v.vendor_ID and c.id = s.id ");
+                        $qry = $conn->query("SELECT * FROM `contract` where vendor_ID != 0");
                         while ($row = $qry->fetch_assoc()):
                             ?>
                             <tr>
                                 <td class="text-center"><?php echo $i++; ?></td>
                                 <td class="text-center"><?php echo date("M d,Y H:i", strtotime($row['date_created'])); ?></td>
-                                <td class="text-center"><?php echo $row['contract_ID'] ?></td>
-                                <td class="align-middle p-1">
-                                    <input type="hidden" name="vendor_ID[]" value="<?php echo $row['vendor_ID'] ?>">
-                                    <input type="text" class="text-center w-100 border-0 vendor_ID" value="<?php echo $row['name'] ?>" required/>
+                                <td class="text-center"><?php echo $row['contract_ID'] ?></td>                             
+                                <td class="text-center"><?php echo $row['vendor_ID'] ?></td>
+                                <td class="text-right"><?php echo $row['startDate'] ?></td>
+                                <td class="text-right"><?php echo $row['endDate'] ?></td>
+
+                                <td align="center">
+                                    <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                                        Action
+                                        <span class="sr-only">Toggle Dropdown</span>
+                                    </button>
+                                    <div class="dropdown-menu" role="menu">
+                                        <a class="dropdown-item" href="?page=contract_mgm/view_conven&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-primary"></span> View</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="?page=contract_mgm/manage_con&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+                                    </div>
                                 </td>
-                                <td class="align-middle p-1">
-                                    <input type="hidden" name="staff_ID[]" value="<?php echo $row['staff_ID'] ?>">
-                                    <input type="text" class="text-center w-100 border-0 staff_ID" value="<?php echo ucwords ($row['name']) ?>" required/>
-                                </td>
-                                <td class="text-right"><?php echo number_format($row['start_date']) ?></td>
-                                <td class="text-right"><?php echo number_format($row['end_date']) ?></td>
-                                
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    
+        <div class="container-fluid">
+            <div class="container-fluid">
+                <table class="table table-hover table-striped">
+                    <colgroup>
+                        <col width="5%">
+                        <col width="10%">
+                        <col width="10%">
+                        <col width="15%">
+                        <col width="10%">
+                        <col width="10%">
+                        <col width="15%">
+                    </colgroup>
+                    <thead>
+                        <p> <strong> Staff List </strong></p>
+                        <tr class="bg-navy disabled">
+                            <th>#</th>
+                            <th>Date Created</th>
+                            <th>Contract ID</th>
+                            <th>Staff ID</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        $qry = $conn->query("SELECT * FROM `contract` where staff_ID != 0");
+                        while ($row = $qry->fetch_assoc()):
+                            ?>
+                            <tr>
+                                <td class="text-center"><?php echo $i++; ?></td>
+                                <td class="text-center"><?php echo date("M d,Y H:i", strtotime($row['date_created'])); ?></td>
+                                <td class="text-center"><?php echo $row['contract_ID'] ?></td>                             
+                                <td class="text-center"><?php echo $row['staff_ID'] ?></td>
+                                <td class="text-right"><?php echo $row['startDate'] ?></td>
+                                <td class="text-right"><?php echo $row['endDate'] ?></td>
+
                                 <td align="center">
                                     <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
                                         Action
@@ -83,10 +188,10 @@
 <script>
     $(document).ready(function () {
         $('.delete_data').click(function () {
-            _conf("Are you sure to delete this PO permanently?", "delete_rent", [$(this).attr('data-id')])
+            _conf("Are you sure to delete this contract permanently?", "delete_rent", [$(this).attr('data-id')])
         })
         $('.view_details').click(function () {
-            uni_modal("Reservaton Details", "purchase_orders/view_con.php?id=" + $(this).attr('data-id'), 'mid-large')
+            uni_modal("Reservaton Details", "contract_mgm/view_con.php?id=" + $(this).attr('data-id'), 'mid-large')
         })
         $('.renew_data').click(function () {
             _conf("Are you sure to renew this rent data?", "renew_rent", [$(this).attr('data-id')]);
---
+++ admin/contract_mgm/manage_con.php
@@ -0,0 +1,179 @@
+<?php if ($_settings->chk_flashdata('success')): ?>
+    <script>
+        alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+    </script>
+<?php endif; ?>
+<?php
+if (isset($_GET['id']) && $_GET['id'] > 0) {
+    $qry = $conn->query("SELECT * from `contract` where id = {$_GET['id']} ");
+    if ($qry->num_rows > 0) {
+        foreach ($qry->fetch_assoc() as $k => $v) {
+            $$k = $v;
+        }
+    }
+}
+?>
+<style>
+    span.select2-selection.select2-selection--single {
+        border-radius: 0;
+        padding: 0.25rem 0.5rem;
+        padding-top: 0.25rem;
+        padding-right: 0.5rem;
+        padding-bottom: 0.25rem;
+        padding-left: 0.5rem;
+        height: auto;
+    }
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox 
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+    [name="tax_percentage"],[name="discount_percentage"]{
+        width:5vw;
+    }*/
+</style>
+<div class="card card-outline card-info">
+    <div class="card-header">
+        <h3 class="card-title"><?php echo isset($id) ? "Update Contract Details" : "New Contract" ?> </h3>
+        <div class="card-tools">
+            <button class="btn btn-sm btn-flat btn-success" id="print" type="button"><i class="fa fa-print"></i> Print</button>
+            <a class="btn btn-sm btn-flat btn-primary" href="?page=contract_mgm/manage_con&id=<?php echo $id ?>">Edit</a>
+            <a class="btn btn-sm btn-flat btn-default" href="?page=contract_mgm">Back</a>
+        </div>
+    </div>
+    <div class="card-body" id="out_print">
+        <div class="row">
+            <div class="col-6 d-flex align-items-center">
+                <div>
+                    <p class="m-0"><?php echo $_settings->info('company_name') ?></p>
+                    <p class="m-0"><?php echo $_settings->info('company_email') ?></p>
+                    <p class="m-0"><?php echo $_settings->info('company_address') ?></p>
+                </div>
+            </div>
+            <div class="col-6">
+                <center><img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" height="200px"></center>
+                <h2 class="text-center"><b>CONTRACT AGREEMENT</b></h2>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-6">
+                <p class="m-0"><h3><b>EMPLOYEE</b></p></h3>
+                <?php
+                $s_qry = $conn->query("SELECT r.*, s.* FROM `contract` r,`staff` s where r.staff_ID  = s.id");
+                $s = $s_qry->fetch_array();
+                ?>
+                <div>
+                    <h5> <b><p class="m-0"><?php echo $s['username'] ?></p>    
+                            <?php
+                            if (['type'] == 1)
+                                echo 'Administrator';
+                            else if (['type'] == 2)
+                                echo 'Manager';
+                            else
+                                echo 'Staff';
+                            ?></b></h5>
+
+                </div>
+            </div>
+
+            <div class="col-6 row">               
+                <div class="col-6">
+                    <p  class="m-0"><b>Date Created</b></p>
+                    <p><b><?php echo date("Y-m-d", strtotime($date_created)) ?></b></p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <h6> <p> WHEREAS the Employer intends to hire the Employee for the Position and the Employee desires to provide their services to the Employer for payment.</p>
+            <p> IN CONSIDERATION of promises and other good and valuable consideration, the parties agree to the following: </p> 
+            <br>
+            <p><strong>I EMPLOYEE DUTIES. .</strong> The Employee agrees that they will act in accordance with this Agreement and with the best interests of the Employer in mind, which may or may not require them to present the best of their skills, experience, and talents, to perform all the duties required of the Position. 
+                <br>In carrying out the duties and responsibilities of their Position, the Employee agrees to adhere to any and all policies, procedures, rules, regulations, as administered by the Employer. 
+                <br>In addition, the Employee agrees to abide by all local, county, State, and Federal laws while employed by the Employer.</p></h6>
+
+        <div class="row">
+            <div class="col-md-12">
+                <STRONG>
+                    <table class="table table-striped table-bordered" id="item-list">
+                        <colgroup>
+                            <col width="20%">
+                            <col width="20%">
+                        </colgroup>
+                        <thead>
+                            <tr class="bg-navy disabled" style="">
+                                <th class="bg-navy disabled text-light px-1 py-2 text-center">Start Date</th>
+                                <th class="bg-navy disabled text-light px-1 py-2 text-center">End Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $rfq_qry = $conn->query("SELECT r.*, s.* FROM `contract` r,`vendor` s where r.vendor_ID  = s.vendor_ID and r.vendor_ID != 0");
+
+                            while ($row = $rfq_qry->fetch_assoc()):
+                                ?>
+                                <tr class=rfq-item" data-id="">
+                                    <td class="align-middle p-2 text-center"><?php echo $row['startDate'] ?></td>
+                                    <td class="align-middle p-0 text-center">
+                                        <input type="date" class="text-center w-100 border-0" value="<?php echo $row['endDate'] ?>"/>
+                                    </td>                            
+                                </tr>
+                                <?php
+                            endwhile;
+                            ?>
+                        </tbody>                        
+                    </table>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="remarks" class="control-label">Company Sign Here:</label>
+                            <img src="uploads/company_sign.PNG" id="remarks" cols="10" rows="4" class="form-control rounded-0"  >
+                        </div>
+                         <div class="col-md-6">
+                            <label for="remarks" class="control-label">Employee Sign Here:</label>
+                            <textarea name="remarks" id="remarks" cols="10" rows="4" class="form-control rounded-0"></textarea>
+                        </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <button class="btn btn-flat btn-primary" form="po-form">Save</button>
+        <a class="btn btn-flat btn-default" href="?page=RFQ">Cancel</a>
+    </div>
+</div>
+<table class="d-none" id="item-clone">
+    <tr class="rfq-item" data-id="">
+        <td class="align-middle p-1 text-center">
+            <button class="btn btn-sm btn-danger py-0" type="button" onclick="rem_item($(this))"><i class="fa fa-times"></i></button>
+        </td> 
+    </tr>
+</table>
+<script>
+    $(function () {
+        $('#print').click(function (e) {
+            e.preventDefault();
+            start_loader();
+            var _h = $('head').clone()
+            var _p = $('#out_print').clone()
+            var _el = $('<div>')
+            _p.find('thead th').attr('style', 'color:black !important')
+            _el.append(_h)
+            _el.append(_p)
+
+            var nw = window.open("", "", "width=1200,height=950")
+            nw.document.write(_el.html())
+            nw.document.close()
+            setTimeout(() => {
+                nw.print()
+                setTimeout(() => {
+                    end_loader();
+                    nw.close()
+                }, 300);
+            }, 200);
+        })
+    })
+</script>
