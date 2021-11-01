5452f9e564f2f63a0cf36ff2471ea5f787e6448b
jiayun1209 <User-PC@LAPTOP-FNMDPHLU.mshome.net>
2021-11-01 09:41:36 +0800

    UPDATE send PO

--- admin/purchase_orders/send_po.php
+++ admin/purchase_orders/send_po.php
@@ -14,11 +14,38 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
 }
 ?>
 <?php
-$sup_qry = $conn->query("SELECT * FROM vendor where vendor_ID = '{$vendor_ID}'");
-                $supplier = $sup_qry->fetch_array();
+$sup_qry = $conn->query("SELECT po.*, s.* from `purchase_order` po inner join `vendor` s on po.vendor_ID  = s.vendor_ID where id = '{$id}'");
+$supplier = $sup_qry->fetch_array();
+$order_items_qry = $conn->query("SELECT o.*,i.name, i.description FROM `purchase_order_details` o inner join inventory i on o.item_id = i.id where o.`po_id` = '$id' ");
+$row = $order_items_qry->fetch_assoc();
+    $comp_name = $_settings->info('company_name');
+    $comp_add = $_settings->info('company_address');
+    $comp_img = $_settings->info('logo');
+    $name = $toEmail = $supplier["name"];
+    $sup_code = $supplier['company_code'];
+    $prod = $supplier['product'];
+    $desc = $supplier['description'];
+    $creation_date = $supplier['date_created'];
+    $date = date("Y-m-d", strtotime($creation_date));
+    $qty = $row['quantity'];
+    $itemName = $row['name'];
+    $itemDesc = $row['description'];
+    $unitPrice = number_format($row['unit_price'],2);
+    $totalPrice = number_format(($row['quantity'] * $row['unit_price']),2);
+    $sub_total = 0;
+    $sub_total += ($row['quantity'] * $row['unit_price']);
+    $discPercent = $supplier['discount_percentage'];
+    $discAmt = number_format($supplier['discount_amount'],2);
+    $taxPercent = number_format($supplier['tax_percentage']);
+    $taxAmt = number_format($supplier['tax_amount'],2);
+    $total = number_format(($sub_total - $discAmt),2);
+    $rem = $supplier['remarks'];
+    $poNum = $supplier['po_no'];
+    
 if (isset($_POST['sendMailBtn'])) {
-    $fromEmail = $_settings->info('company_name');
-    $toEmail = $supplier["email"];
+    $fromEmail = $_settings->info('company_email');
+    $toEmail = $supplier['email'];
+
 
     $to = "$toEmail";
     $subject = "PO";
@@ -35,6 +62,105 @@ if (isset($_POST['sendMailBtn'])) {
 				<title>Document</title>
 			</head>
 			<body>
+                        <div class="card card-outline card-info">
+    <div class="card-header">
+        <h3 class="card-title">Purchase Order Details</h3>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-6 d-flex align-items-center">
+                <div>
+                    <p class="m-0">Company Name: '.$comp_name.'</p>
+                    <p class="m-0">Email address: '.$fromEmail.'</p>
+                    <p class="m-0">Delivery address:'.$comp_add.'</p>
+                </div>
+            </div>
+            <div class="col-6">
+                <center><img src="'.$comp_img.'" alt="" height="200px"></center>
+                <h2 class="text-center"><b>PURCHASE ORDER</b></h2>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-6">
+                <p class="m-0"><b>Vendor Details</b></p>
+                <div>
+                    <p>Vendor Name     : '.$name.'</p>
+                    <p>Company Code    : '.$sup_code.'</p>                   
+                    <p>Email address   :'.$toEmail.'</p>
+                    <p>Product offered :'.$product.' </p>
+                    <p>Description     : '.$desc.'</p>
+                </div>
+            </div>
+            <div class="col-6 row">
+                <div class="col-6">
+                    <p  class="m-0"><b>PO Number: '.$poNum.'</b></p>
+                </div>
+                <div class="col-6">
+                    <p  class="m-0"><b>Date Created: '.$date.'</b></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped table-bordered">
+                    <colgroup>
+                        <col width="10%">
+                        <col width="20%">
+                        <col width="30%">
+                        <col width="15%">
+                        <col width="15%">
+                    </colgroup>
+                    <thead>
+                        <tr class="bg-navy disabled" style="">
+                            <th class="bg-navy disabled text-light px-1 py-1 text-center">Qty</th>
+                            <th class="bg-navy disabled text-light px-1 py-1 text-center">Item</th>
+                            <th class="bg-navy disabled text-light px-1 py-1 text-center">Description</th>
+                            <th class="bg-navy disabled text-light px-1 py-1 text-center">Price</th>
+                            <th class="bg-navy disabled text-light px-1 py-1 text-center">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                                <tr class="po-item" data-id="">
+                                    <td class="align-middle p-0 text-center">'.$qty.'</td>
+                                    <td class="align-middle p-1">'.$itemName.'</td>
+                                    <td class="align-middle p-1 item-description">'.$itemDesc.'</td>
+                                    <td class="align-middle p-1">'.$unitPrice.'</td>
+                                    <td class="align-middle p-1 text-right total-price">'.$totalPrice.'</td>
+                                </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr class="bg-lightblue">
+                        <tr>
+                            <th class="p-1 text-right" colspan="4">Sub Total</th>
+                            <th class="p-1 text-right">'.$sub_total.'</th>
+                        </tr>
+                        <tr>
+                            <th class="p-1 text-right" colspan="4">Discount ('.$discPercent.'%)
+                            </th>
+                            <th class="p-1 text-right">'.$discAmt.'</th>
+                        </tr>
+                        <tr>
+                            <th class="p-1 text-right" colspan="4">Tax Inclusive ('.$taxPercent.'%)</th>
+                            <th class="p-1 text-right">'.$taxAmt.'</th>
+                        </tr>
+                        <tr>
+                            <th class="p-1 text-right" colspan="4">Total</th>
+                            <th class="p-1 text-right">'.$total.'</th>
+                        </tr>
+                        </tr>
+                    </tfoot>
+                </table>
+                <div class="row">
+                    <div class="col-6">
+                        <label>Remarks</label>
+                        <p>'.$remarks.'</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
 			<br/>
                     Regards<br/>
                   '.$fromEmail.'
@@ -43,7 +169,7 @@ if (isset($_POST['sendMailBtn'])) {
 			</html>';
     $result = @mail($to, $subject, $message, $headers);
 
-    echo '<script>alert("Email sent successfully !")</script>';
+    echo '<script>alert("The PO has been sent successfully to the supplier!")</script>';
 }
 
 ?>
