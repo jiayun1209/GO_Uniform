a0fcb2bd36013259ff0e2a9f657b83b401996bb4
meekei <meekei@mailer.blueoceanit.com.my>
2021-11-27 16:10:41 +0800

    update po part

--- admin/purchase_orders/index.php
+++ admin/purchase_orders/index.php
@@ -75,7 +75,6 @@
                                 <td align="center">
                                     <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
                                         Action
-                                        
                                         <span class="sr-only">Toggle Dropdown</span>
                                     </button>
                                     <div class="dropdown-menu" role="menu">
